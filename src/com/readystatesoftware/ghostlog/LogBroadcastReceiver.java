5a3a027c355cb56c32e44851ed76380d2d656b52
liushiwei <bndxgae@163.com>
2016-06-22 10:31:42 +0800

    add auto save log

--- src/com/readystatesoftware/ghostlog/LogBroadcastReceiver.java
+++ src/com/readystatesoftware/ghostlog/LogBroadcastReceiver.java
@@ -4,7 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
 
 public class LogBroadcastReceiver extends BroadcastReceiver {
 
@@ -15,6 +18,32 @@ public class LogBroadcastReceiver extends BroadcastReceiver {
 			if(mPrefs.getBoolean(context.getString(R.string.log_auto_save), false)){
 				context.startService(new Intent(context, LogSaveService.class));
 			};
+		}
+		if (intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED)) {
+			Uri uri = intent.getData();
+			 String path = uri.getPath();
+	         Log.e("LogViewer", "----------MOUNT: "+path.substring(path.lastIndexOf('/')));
+	         SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+	         String pathString = mPrefs.getString(context.getString(R.string.pref_log_path), "");
+	         Log.e("LogViewer", "----------save path: "+pathString.substring(pathString.lastIndexOf('/')));
+	         if(!pathString.substring(pathString.lastIndexOf('/')).equals(path.substring(path.lastIndexOf('/')))){
+	        	 return;
+	         }
+	         if(mPrefs.getBoolean(context.getString(R.string.pref_log_auto_save), false)){
+	        		 context.startService(new Intent(context, LogSaveService.class));
+	         };
+			
+		}
+		if (intent.getAction().equals(Intent.ACTION_MEDIA_EJECT)) {
+			Uri uri = intent.getData();
+			 String path = uri.getPath();
+	         Log.e("LogViewer", "----------EJECT: "+path);
+	         SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+	         String pathString = mPrefs.getString(context.getString(R.string.pref_log_path), "");
+	         if(!pathString.substring(pathString.lastIndexOf('/')).equals(path.substring(path.lastIndexOf('/')))){
+	        	 return;
+	         }
+	         context.stopService(new Intent(context, LogSaveService.class));
 		}
 
 	}
--- src/com/readystatesoftware/ghostlog/LogSaveService.java
+++ src/com/readystatesoftware/ghostlog/LogSaveService.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.text.format.Time;
@@ -33,98 +34,103 @@ public class LogSaveService extends Service {
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		
+
 		return null;
 	}
-	
+
 	LogReaderAsyncTask mLogReaderAsyncTask;
 	String mSaveFile;
+
 	@Override
 	public void onCreate() {
-		
-		SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+		SharedPreferences mPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
 		// 创建一个通知
-	    Notification mNotification = new Notification();
+		Notification mNotification = new Notification();
 
-	    // 设置属性值
-	    mNotification.icon = R.drawable.ic_launcher;
-	    mNotification.tickerText = "Log Saving";
-	    mNotification.when = System.currentTimeMillis(); // 立即发生此通知
+		// 设置属性值
+		mNotification.icon = R.drawable.ic_launcher;
+		mNotification.tickerText = "Log Saving";
+		mNotification.when = System.currentTimeMillis(); // 立即发生此通知
 
-	    // 带参数的构造函数,属性值如上
-	    // Notification mNotification = = new Notification(R.drawable.icon,"NotificationTest", System.currentTimeMillis()));
+		// 带参数的构造函数,属性值如上
+		// Notification mNotification = = new
+		// Notification(R.drawable.icon,"NotificationTest",
+		// System.currentTimeMillis()));
 
-	    // 添加声音效果
-	    mNotification.defaults |= Notification.DEFAULT_SOUND;
+		// 添加声音效果
+		mNotification.defaults |= Notification.DEFAULT_SOUND;
 
-	    // 添加震动,后来得知需要添加震动权限 : Virbate Permission
-	    //mNotification.defaults |= Notification.DEFAULT_VIBRATE ; 
+		// 添加震动,后来得知需要添加震动权限 : Virbate Permission
+		// mNotification.defaults |= Notification.DEFAULT_VIBRATE ;
 
-	    //添加状态标志 
+		// 添加状态标志
 
-	    //FLAG_AUTO_CANCEL          该通知能被状态栏的清除按钮给清除掉
-	    //FLAG_NO_CLEAR                 该通知能被状态栏的清除按钮给清除掉
-	    //FLAG_ONGOING_EVENT      通知放置在正在运行
-	    //FLAG_INSISTENT                通知的音乐效果一直播放
-	    mNotification.flags = Notification.FLAG_ONGOING_EVENT ;
+		// FLAG_AUTO_CANCEL 该通知能被状态栏的清除按钮给清除掉
+		// FLAG_NO_CLEAR 该通知能被状态栏的清除按钮给清除掉
+		// FLAG_ONGOING_EVENT 通知放置在正在运行
+		// FLAG_INSISTENT 通知的音乐效果一直播放
+		mNotification.flags = Notification.FLAG_ONGOING_EVENT;
 
-	    //将该通知显示为默认View
-	    Time time = new Time();
-	    String pathString = mPrefs.getString(getString(R.string.pref_log_path), "/mnt/sdcard");
-	    DateFormat df5 = new SimpleDateFormat("yyyy-MM-dd-hh-mm", Locale.US); 
-	    
-	    mSaveFile = pathString+"/log_"+df5.format(new Date())+".txt";
-	    mNotification.setLatestEventInfo(this, "Log Saving", "Save to "+mSaveFile,null);
-	    
-	    // 设置setLatestEventInfo方法,如果不设置会App报错异常
-	    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-	    
-	    //注册此通知 
-	    // 如果该NOTIFICATION_ID的通知已存在，会显示最新通知的相关信息 ，比如tickerText 等
-	    mNotificationManager.notify(2, mNotification);
-	    Log.e("TAG", "START Service show Notification");
-	    mLogReaderAsyncTask = new LogReaderAsyncTask();
-	    mLogReaderAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+		// 将该通知显示为默认View
+		Time time = new Time();
+		String pathString = mPrefs.getString(getString(R.string.pref_log_path),
+				"/mnt/sdcard");
+		DateFormat df5 = new SimpleDateFormat("yyyy-MM-dd-hh-mm", Locale.US);
 
-	    
+		mSaveFile = pathString + "/log_" + df5.format(new Date()) + ".txt";
+		mNotification.setLatestEventInfo(this, "Log Saving", "Save to "
+				+ mSaveFile, null);
+
+		// 设置setLatestEventInfo方法,如果不设置会App报错异常
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+		// 注册此通知
+		// 如果该NOTIFICATION_ID的通知已存在，会显示最新通知的相关信息 ，比如tickerText 等
+		mNotificationManager.notify(2, mNotification);
+		Log.e("TAG", "START Service show Notification");
+		mLogReaderAsyncTask = new LogReaderAsyncTask();
+		mLogReaderAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
 	}
-	 private void stopLogReader() {
-	        if (mLogReaderAsyncTask != null) {
-	        	mLogReaderAsyncTask.cancel(true);
-	        }
-	        mLogReaderAsyncTask = null;
-	        Log.i("LogSaveService", "Log reader task stopped");
+
+	private void stopLogReader() {
+		if (mLogReaderAsyncTask != null) {
+			mLogReaderAsyncTask.cancel(true);
+		}
+		mLogReaderAsyncTask = null;
+		Log.i("LogSaveService", "Log reader task stopped");
 	}
 
 	@Override
 	public void onDestroy() {
-		 NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		    // 取消的只是当前Context的Notification
-		 mNotificationManager.cancel(2);
-		 stopLogReader();
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		// 取消的只是当前Context的Notification
+		mNotificationManager.cancel(2);
+		stopLogReader();
 		super.onDestroy();
 	}
-	
-	
-	public static boolean getSaveLogToFile(String buffer,String fileName) {
+
+	public static boolean getSaveLogToFile(String buffer, String fileName) {
 		Process dumpLogcatProcess = null;
 		BufferedReader reader = null;
 		String result = null;
 		try {
-			
+
 			List<String> args = LogcatHelper.getLogcatArgs(buffer);
 			args.add("-d"); // -d just dumps the whole thing
-			
+
 			dumpLogcatProcess = RuntimeHelper.exec(args);
-			reader = new BufferedReader(new InputStreamReader(dumpLogcatProcess
-					.getInputStream()), 8192);
+			reader = new BufferedReader(new InputStreamReader(
+					dumpLogcatProcess.getInputStream()), 8192);
 			File outPutFile = new File(fileName);
-			if(!outPutFile.exists()){
-				if(!outPutFile.createNewFile())
+			if (!outPutFile.exists()) {
+				if (!outPutFile.createNewFile())
 					return false;
 			}
 			FileOutputStream outputStream = new FileOutputStream(outPutFile);
-			
+
 			String line;
 			while ((line = reader.readLine()) != null) {
 				outputStream.write(line.getBytes());
@@ -134,15 +140,15 @@ public class LogSaveService extends Service {
 			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {		
+		} finally {
 			if (dumpLogcatProcess != null) {
 				RuntimeHelper.destroy(dumpLogcatProcess);
-				Log.d("LogSaveService","destroyed 1 dump logcat process");
+				Log.d("LogSaveService", "destroyed 1 dump logcat process");
 			}
 			// post-jellybean, we just kill the process, so there's no need
-	        // to close the bufferedReader.  Anyway, it just hangs.
-	        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN
-	                && reader != null) {
+			// to close the bufferedReader. Anyway, it just hangs.
+			if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN
+					&& reader != null) {
 				try {
 					reader.close();
 				} catch (IOException e) {
@@ -151,62 +157,63 @@ public class LogSaveService extends Service {
 			}
 		}
 		return false;
-		
+
 	}
+
 	class LogReaderAsyncTask extends AsyncTask<Integer, LogLine, Boolean> {
-		 Process process = null;
-	      BufferedReader reader = null;
-	      boolean ok = true;
+		Process process = null;
+		BufferedReader reader = null;
+		boolean ok = true;
+
 		@Override
 		protected Boolean doInBackground(Integer... params) {
-			   try {
-				  
+			try {
 
-		            process = LogcatHelper.getLogcatProcess("main");
-		            reader = new BufferedReader(new InputStreamReader(process.getInputStream()), 8192);
-		            File outPutFile = new File(mSaveFile);
-					if(!outPutFile.exists()){
-						if(!outPutFile.createNewFile())
-							return false;
+				process = LogcatHelper.getLogcatProcess("main");
+				reader = new BufferedReader(new InputStreamReader(
+						process.getInputStream()), 8192);
+				File outPutFile = new File(mSaveFile);
+				if (!outPutFile.exists()) {
+					if (!outPutFile.createNewFile())
+						return false;
+				}
+				FileOutputStream outputStream = new FileOutputStream(outPutFile);
+
+				String line;
+				while (!isCancelled() && (line = reader.readLine()) != null) {
+					outputStream.write(line.getBytes());
+					outputStream.write('\n');
+				}
+				outputStream.close();
+
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				ok = false;
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				ok = false;
+
+			} finally {
+
+				if (process != null) {
+					RuntimeHelper.destroy(process);
+				}
+
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN
+						&& reader != null) {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
-					FileOutputStream outputStream = new FileOutputStream(outPutFile);
-					
-					String line;
-					while (!isCancelled()&&(line = reader.readLine()) != null) {
-						outputStream.write(line.getBytes());
-						outputStream.write('\n');
-					}
-					outputStream.close();
+				}
 
-		        } catch (IOException e) {
-		        	Toast.makeText(LogSaveService.this, "保存出错了！！", Toast.LENGTH_SHORT).show();
-		            e.printStackTrace();
-		            ok = false;
-
-		        }catch (Exception e) {
-		        	Toast.makeText(LogSaveService.this, "保存出错了！！", Toast.LENGTH_SHORT).show();
-		            e.printStackTrace();
-		            ok = false;
-
-		        } finally {
-
-		            if (process != null) {
-		                RuntimeHelper.destroy(process);
-		            }
-
-		            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN
-		                    && reader != null) {
-		                try {
-		                    reader.close();
-		                } catch (IOException e) {
-		                    e.printStackTrace();
-		                }
-		            }
-
-		        }
+			}
 			return null;
 		}
-		
+
 	}
 
 }
